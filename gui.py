08268d8256f659f4af406aac5603ccc54711117e
marhcouto <marhcouto1101@gmail.com>
2021-07-18 16:31:23 +0100

    Updated initial interface look

--- gui.py
+++ gui.py
@@ -1,10 +1,19 @@
 from tkinter import *
 
-gui = Tk()
-gui.geometry("1000x500")
-myLabel = Label(gui, text="Hello world")
 
-myLabel.pack()
+gui = Tk()
+gui.geometry("500x200")
+
+myLabel = Label(gui, text = "Welcome to Problem Sorter")
+
+insertButton = Button(gui, text = "Insert Problems")
+searchButton = Button(gui, text = "Search Problems")
+deleteButton = Button(gui, text = "Delete Problems")
+
+myLabel.place(relx = 0.5, rely = 0.1, anchor = CENTER)
+insertButton.place(relx = 0.2, rely = 0.9, anchor = CENTER)
+searchButton.place(relx = 0.5, rely = 0.9, anchor = CENTER)
+deleteButton.place(relx = 0.8, rely = 0.9, anchor = CENTER)
 
 gui.mainloop()
 
