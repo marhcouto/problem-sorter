592f7d65f214281a15b35f19a5a9de3b495b1bd7
marhcouto <marhcouto1101@gmail.com>
2021-07-18 15:41:47 +0100

    First steps

---
+++ db_inter.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+connection = sqlite3.connect("problems.db")
+
+cursor = connection.cursor()
+cursor.execute("PRAGMA foreign_keys = on")
+for row in cursor.execute("SELECT * FROM Problem;"):
+    print(row)
+
+
+print(list(cursor.execute("SELECT * FROM Problem;")))
+
+
+try:
+    cursor.execute("INSERT INTO ProblemTheme (problemId, themeId) VALUES(100, 100);")
+except sqlite3.IntegrityError:
+    print("UNABLE TO PERFORM OPERATION")
+else:
+    print("ALL OK")
+finally:
+    print("I HAPPEN ANYWAY")
+
---
+++ gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+gui = Tk()
+gui.geometry("1000x500")
+myLabel = Label(gui, text="Hello world")
+
+myLabel.pack()
+
+gui.mainloop()
+
