bcf90224f75362a66c40241c11d30bd0df5f0bc5
marhcouto <marhcouto1101@gmail.com>
2021-07-18 18:51:32 +0100

    Starting other menus

--- gui.py
+++ gui.py
@@ -1,19 +1,61 @@
 from tkinter import *
 
+class Menu:
+    def __init__(self, gui):
+        self.gui = gui
 
-gui = Tk()
-gui.geometry("500x200")
+    @property
+    def gui(self):
+        return self.__gui
 
-myLabel = Label(gui, text = "Welcome to Problem Sorter")
+    @gui.setter
+    def gui(self, newGui):
+        self.__gui = newGui
 
-insertButton = Button(gui, text = "Insert Problems")
-searchButton = Button(gui, text = "Search Problems")
-deleteButton = Button(gui, text = "Delete Problems")
+    def place(self):
+        pass
 
-myLabel.place(relx = 0.5, rely = 0.1, anchor = CENTER)
-insertButton.place(relx = 0.2, rely = 0.9, anchor = CENTER)
-searchButton.place(relx = 0.5, rely = 0.9, anchor = CENTER)
-deleteButton.place(relx = 0.8, rely = 0.9, anchor = CENTER)
+    def unplace(self):
+        pass
 
-gui.mainloop()
+class InsertMenu(Menu):
 
+    def __init__(self, gui):
+        super().__init__(gui)
+    
+    def build():
+        pass
+
+class MainMenu(Menu):
+
+    def __init__(self, gui):
+        super().__init__(gui)
+        self.insertButton = Button(self.gui, text = "Insert Problems")
+        self.searchButton = Button(self.gui, text = "Search Problems")
+        self.deleteButton = Button(self.gui, text = "Delete Problems")
+        self.myLabel = Label(self.gui, text = "Welcome to Problem Sorter")
+        
+    
+    def place(self):
+        self.myLabel.place(relx = 0.5, rely = 0.1, anchor = CENTER)
+        self.insertButton.place(relx = 0.2, rely = 0.9, anchor = CENTER)
+        self.searchButton.place(relx = 0.5, rely = 0.9, anchor = CENTER)
+        self.deleteButton.place(relx = 0.8, rely = 0.9, anchor = CENTER)
+
+    def unplace(self):
+        self.myLabel.place_forget()
+        self.insertButton.place_forget()
+        self.deleteButton.place_forget()
+        self.searchButton.place_forget()
+
+
+def main():
+    gui = Tk()
+    gui.geometry("500x200")
+    mainMenu = MainMenu(gui)
+
+    mainMenu.place()
+    gui.mainloop()
+
+if __name__ == "__main__":
+    main()
